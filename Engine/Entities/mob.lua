178b4e52f033f602cd6cada54db6bfae311b6acd
xTheLifex <nicholastoya@yahoo.com.br>
2023-08-31 10:29:15 -0300

    Simple mob move test

--- Engine/Entities/mob.lua
+++ Engine/Entities/mob.lua
@@ -1,18 +1,15 @@
 local ent = {}
 ent.base = "base"
-ent.sprite = engine.assets.graphics.Simple["meow"]
 ent.nextDirChange = 0
-ent.dir = { x = 0, y = 0 }
-ent.speed = 5
-
+ent.dir = { x = 0.0, y = 0.0 }
+ent.speed = 12
 function ent:OnUpdate(deltaTime)
+    self.x = self.x + (self.speed * deltaTime * self.dir.x)
+    self.y = self.y + (self.speed * deltaTime * self.dir.y)
     if (self.nextDirChange <= CurTime()) then
         self.nextDirChange = CurTime() + love.math.random(5,20) * 0.1
-        self.dir = { x = love.math.random(-1,1), y = love.math.random(-1,1)}
+        self.dir = { x = love.math.random(-1.0,1.0), y = love.math.random(-1.0,1.0)}
     end
-
-    self.x = self.x + ent.dir.x * self.speed * deltaTime
-    self.y = self.y + ent.dir.y * self.speed * deltaTime
 end
 
 function ent:OnDraw()
