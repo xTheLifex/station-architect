46d25f1d60b7e4950ab386717bc358f226e7c180
xTheLifex <nicholastoya@yahoo.com.br>
2023-10-24 09:54:45 -0300

    Physics Changes

--- Engine/physics.lua
+++ Engine/physics.lua
@@ -7,71 +7,92 @@ engine.physics.colliding = {}
 --                                External Use                                --
 -- -------------------------------------------------------------------------- --
 
+-- Helper function to check if an entity is within a circle
+local function isEntityInCircle(ent, x, y, radius, conly, filter)
+    if ent.collider then
+        if ent.collider.type == "circle" then
+            local centerDistance = utils.Distance(ent.x, ent.y, x, y)
+            if centerDistance < ent.collider.radius + radius then
+                if filter == nil or filter(ent) then
+                    return true
+                end
+            end
+        elseif ent.collider.type == "edge" then
+            -- TODO
+        end
+    elseif conly and utils.Distance(ent.x, ent.y, x, y) < radius then
+        if filter == nil or filter(ent) then
+            return true
+        end
+    end
+    return false
+end
+
 -- Returns the first entity found in the circle or false if none is found.
-engine.physics.CircleCheck = function(x,y, radius, colliderOnly, filter)
+engine.physics.CircleCheck = function(x, y, radius, colliderOnly, filter)
     local conly = colliderOnly or true
-    
-    for _,ent in ipairs(engine.world.entities) do
-        if (ent.collider) then
-            if (ent.collider.type == "circle") then
-                local centerDistance = utils.Distance(ent.x, ent.y, x, y)
-                            
-                -- If the distance between their centers is less than the sum of their radiuses...
-                if (centerDistance < ent.collider.radius + radius) then
-                    if (filter ~= nil and filter(ent) == true) then
-                        return ent
-                    end
-                end
-            elseif(ent.collider.type == "edge") then
-                -- TODO
-            end
-        else
-            if (conly and utils.Distance(ent.x,ent.y,x,y) < radius) then
-                if (filter ~= nil and filter(ent) == true) then
-                    return ent
-                end
-            end
+
+    for _, ent in ipairs(engine.world.entities) do
+        if isEntityInCircle(ent, x, y, radius, conly, filter) then
+            return ent
         end
     end
     return false
 end
 
 -- Returns all entities found in the circle.
-engine.physics.CircleCheckAll = function(x,y, radius, colliderOnly, filter)
+engine.physics.CircleCheckAll = function(x, y, radius, colliderOnly, filter)
     local conly = colliderOnly or true
     local results = {}
-    
-    for _,ent in ipairs(engine.world.entities) do
-        if (ent.collider) then
-            if (ent.collider.type == "circle") then
-                local centerDistance = utils.Distance(ent.x, ent.y, x, y)
-                            
-                -- If the distance between their centers is less than the sum of their radiuses...
-                if (centerDistance < ent.collider.radius + radius) then
-                    if (filter ~= nil and filter(ent) == true) then
-                        table.insert(results,ent)
-                    end
-                end
-            elseif(ent.collider.type == "edge") then
-                -- TODO
-            end
-        else
-            if (conly and utils.Distance(ent.x,ent.y,x,y) < radius) then
-                if (filter ~= nil and filter(ent) == true) then
-                    table.insert(results,ent)
-                end
-            end
+
+    for _, ent in ipairs(engine.world.entities) do
+        if isEntityInCircle(ent, x, y, radius, conly, filter) then
+            table.insert(results, ent)
         end
     end
     return results
 end
-
 
 -- -------------------------------------------------------------------------- --
 --                                  Internal                                  --
 -- -------------------------------------------------------------------------- --
 
 engine.AddCVar("debug_physics", false, "Enables the display of debug physical information")
+
+engine.physics.CheckCollisions = function()
+    local ents = engine.entities.GetAll()
+    local collisions = {}
+    engine.physics.colliding = {}
+
+    for _, a in ipairs(ents) do
+        if a.collider and a.collider.type == "circle" then
+            for _, b in ipairs(ents) do
+                if a ~= b and b.collider and b.collider.type == "circle" then
+                    -- Collision between two circle colliders
+                    local centerDistance = utils.Distance(a.x, a.y, b.x, b.y)
+
+                    if centerDistance < a.collider.radius + b.collider.radius then
+                        -- Collision!
+                        collisions[a] = collisions[a] or {}
+                        if not table.contains(collisions[a], b) then
+                            table.insert(collisions[a], b)
+                        end
+                        if not table.contains(engine.physics.colliding, a) then
+                            table.insert(engine.physics.colliding, a)
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+    engine.physics.collisions = collisions
+end
+
+
+
+-- ----------------------------- ! DEPRECATED ! ----------------------------- --
+--[[
 
 engine.physics.CheckCollisions = function()
     local ents = engine.entities.GetAll()
@@ -95,7 +116,6 @@ engine.physics.CheckCollisions = function()
                         -- -------------------------------------------------------------------------- --
                         if (b.collider.type == "edge") then
                             -- --------------------- Collision between edge and edge -------------------- --
-
 
                             -- ------------------------------------ - ----------------------------------- --
                         elseif(b.collider.type == "circle") then
@@ -160,30 +180,39 @@ engine.physics.CheckCollisions = function()
     end
 
     engine.physics.collisions = collisions
-end
+end]]
+-- ----------------------------- ! DEPRECATED ! ----------------------------- --
 
 -- engine.world.IsWithinBoundaries
 
 engine.physics.ResolveCollisions = function()
     for a,targets in pairs(engine.physics.collisions) do
-        for _,b in ipairs(targets) do
+        for _, b in pairs(targets) do
             engine.physics.RepelObjects(a,b)
         end
     end
 end
 
 engine.physics.RepelObjects = function(a, b)
+
+    assert(isent(a), "Invalid entity for collision.")
+    assert(isent(b), "Invalid entity for collision.")
+
     -- Calculate the direction from a to b
     local direction_ab = {x = b.x - a.x, y = b.y - a.y}
     local distance = math.sqrt(direction_ab.x * direction_ab.x + direction_ab.y * direction_ab.y)
+
+    local na = #engine.physics.collisions[a] or 0
+    local nb = #engine.physics.collisions[b] or 0
+    local num_of_collisions = na + nb
 
     -- Calculate the overlap based on the sum of radii
     local overlap = a.collider.radius + b.collider.radius - distance
 
     if overlap > 0 then
         -- Calculate the repel force based on the overlap
-        local repel_force = overlap * 0.015
-
+        local repel_force = overlap * (0.005 + (0.0001 * num_of_collisions))
+        
         -- Normalize the direction
         direction_ab.x = direction_ab.x / distance
         direction_ab.y = direction_ab.y / distance
@@ -263,7 +292,7 @@ hooks.Add("PostGameDraw", function()
         if (ent.collider.type == "circle") then
             love.graphics.circle("line", ent.x, ent.y, ent.collider.radius)
         elseif(ent.collider.type == "edge") then
-
+            -- TODO
         end
 
         love.graphics.setColor(1,1,1)
