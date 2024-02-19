c1bfc4680a4c45f693833908798122e3c9400268
xTheLifex <nicholastoya@yahoo.com.br>
2024-02-18 22:37:20 -0300

    ENEMY CHASER

--- Engine/Entities/mob.lua
+++ Engine/Entities/mob.lua
@@ -10,28 +10,53 @@ ent.collider = {
     radius = 16
 }
 
+function ent:OnCreate()
+    self.static = (math.random(0,5) ~= 0)
+end
+
 function ent:OnUpdate(deltaTime)
-    self.x = self.x + (self.speed * deltaTime * self.dir.x)
-    self.y = self.y + (self.speed * deltaTime * self.dir.y)
-    if (self.nextDirChange <= CurTime()) then
-        self.nextDirChange = CurTime() + love.math.random(5,20) * 0.1
-        self.dir = { x = love.math.random(-1.0,1.0), y = love.math.random(-1.0,1.0)}
+    if not self.static then
+        self.x = self.x + (self.speed * deltaTime * self.dir.x)
+        self.y = self.y + (self.speed * deltaTime * self.dir.y)
+        if (self.nextDirChange <= CurTime()) then
+            self.nextDirChange = CurTime() + love.math.random(5,20) * 0.1
+            self.dir = { x = love.math.random(-1.0,1.0), y = love.math.random(-1.0,1.0)}
+        end
     end
 
-    if (meow ~= nil and meow.master ~= nil) then
-        if (meow.master.id == self.id) then
-            self.tint = 1
-        else
-            local dist = engine.entities.Distance(meow.master, self)
+    if (self.target) then
+        local ent = engine.entities.GetByID(self.target)
+        local dist = engine.entities.Distance(self, ent)
 
-            local tint = 1/( ( (dist/1000) +1)^2 )
-            self.tint = tint
+        if (dist > 80) then
+            self.target = nil
+        end
+
+        -- set this entity's dir to point towards target
+        local angle = math.atan2(ent.y - self.y, ent.x - self.x)
+        self.dir = { x = math.cos(angle), y = math.sin(angle) }
+    else
+        local all_near = engine.entities.GetOnRadius(self.x, self.y, 80)
+        local possible_targets = {}
+        
+        for _,ent in pairs(all_near) do
+            if (ent.static == true) then
+                table.insert(possible_targets, ent)
+            end
+        end
+
+        if possible_targets and possible_targets[1] then
+            self.target = possible_targets[1].id
         end
     end
 end
 
 function ent:OnDraw()
-    love.graphics.setColor(self.tint,1-self.tint,1-self.tint)
+    if self.static then
+        love.graphics.setColor(0,0,0.5)
+    else
+        love.graphics.setColor(self.tint,1-self.tint,1-self.tint)
+    end
 	self:DrawSelf()
 end
 
--- Game/game.lua
+++ Game/game.lua
@@ -19,20 +19,25 @@ hooks.Add("OnGameLoad", function()
 		end
 	end
 
-	for i=0,20 do
+	for i=5,20 do
 		engine.entities.Create("mob", {
-			x = 8 * i ,
-			y = 8 * i
+			x = 8 + i * math.random(0,8),
+			y = 8 + i * math.random(0,8)
 		})
 	end
-
-	local mobs = engine.entities.GetByType("mob")
-
-	meow = meow or {}
-	meow.master = mobs[love.math.random(1,#mobs)]
 	
 end)
 
 hooks.Add("OnGameDraw", function ()	
-	
+	for _,ent in pairs(engine.entities.GetByType("mob")) do
+		if (ent.static == false) then
+			love.graphics.setColor(1,0,1,0.25)
+			love.graphics.circle("line", ent.x, ent.y, 80)
+
+			love.graphics.setColor(0,1,1)
+			local t = engine.entities.GetByID(ent.target)
+			if t then love.graphics.line(ent.x, ent.y, t.x, t.y) end
+			love.graphics.setColor(1,1,1)
+		end
+	end
 end)
