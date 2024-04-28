a4067e0f9c6f442417936452b9e129e56e39ee8b
xTheLifex <nicholastoya@yahoo.com.br>
2024-04-28 17:38:15 -0300

    Adjust Scale

--- Engine/misc.lua
+++ Engine/misc.lua
@@ -10,6 +10,6 @@ hooks.Add("OnEngineDraw", function ()
     if (engine.GetCVar("showfps", false) == true) then
         local avg = love.timer.getAverageDelta()
         local s = string.format("FPS: %s, AVG: %s", math.floor(engine.fps), math.floor(1/avg))
-        love.graphics.print(s, 0, 0, 0, 1.125,1.125)
+        love.graphics.print(s, 0, 0, 0, 1,1)
     end
 end)
