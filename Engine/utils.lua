7f962b83f8a86be1d8f9eeafcd9bb2f77471afa5
xTheLifex <nicholastoya@yahoo.com.br>
2024-05-16 11:25:54 -0300

    Pick utility

--- Engine/utils.lua
+++ Engine/utils.lua
@@ -6,6 +6,82 @@ utils = utils or {}
 --                                    Misc                                    --
 -- -------------------------------------------------------------------------- --
 
+function utils.Pick2(options, weights)
+    if not options or #options == 0 then
+        return nil
+    end
+    
+    -- If weights are not provided, assume equal chances for all options
+    if not weights then
+        return options[math.random(1, #options)]
+    end
+    
+    -- Calculate the total weight
+    local totalWeight = 0
+    for _, weight in ipairs(weights) do
+        totalWeight = totalWeight + weight
+    end
+    
+    -- Generate a random number between 0 and the total weight
+    local randomValue = math.random() * totalWeight
+    
+    -- Find the option corresponding to the random value
+    local cumulativeWeight = 0
+    for i, weight in ipairs(weights) do
+        cumulativeWeight = cumulativeWeight + weight
+        if randomValue <= cumulativeWeight then
+            return options[i]
+        end
+    end
+    
+    -- This should never happen, but just in case
+    return nil
+end
+
+
+function utils.Pick(options)
+    if not options or next(options) == nil then
+        return nil
+    end
+
+    -- Extract keys and values from the options table
+    local keys = {}
+    local values = {}
+    for key, value in pairs(options) do
+		if not (isnumber(value)) then
+			return utils.Pick2(options)
+		end
+        table.insert(keys, key)
+        table.insert(values, value or 1)
+    end
+
+    -- Calculate the total weight
+    local totalWeight = 0
+    for _, weight in ipairs(values) do
+        totalWeight = totalWeight + weight
+    end
+
+    -- Generate a random number between 0 and the total weight
+    local randomValue = math.random() * totalWeight
+
+    -- Find the option corresponding to the random value
+    local cumulativeWeight = 0
+    for i, weight in ipairs(values) do
+        cumulativeWeight = cumulativeWeight + weight
+        if randomValue <= cumulativeWeight then
+            return keys[i]
+        end
+    end
+
+    -- This should never happen, but just in case
+    return nil
+end
+
+
+pick = utils.Pick
+Pick = utils.Pick
+
+
 function utils.ScreenX()
 	return love.graphics.getWidth()
 end
